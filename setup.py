b1fd3e69cac3985b901b48dbe6c9df2536e42c2e
Dr Alex Stead <a.d.stead@leeds.ac.uk>
2023-08-14 00:49:39 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="FronPy",
+    name="fronpy",
     version="0.0.0",
     description="Stochastic frontier analysis in Python",
     author="Alexander D. Stead",
     author_email="a.d.stead@leeds.ac.uk",
     include_package_data=True,
-    package_data={'': ['data/*.csv']},
+    package_data={'fronpy': ['data/*.csv']},
     packages=find_packages(),
     install_requires=[
         "numpy>=1.0",
